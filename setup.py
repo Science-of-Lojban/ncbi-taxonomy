961aefabacb449b552072c9b360c773db2d932d4
Gleki Arxokuna <gleki.is.my.name@gmail.com>
2017-04-15 20:13:33 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='NCBI-Lojban',
+      version='0.0.0.1',
+      description='NCBI taxonomy for Lojban language',
+      url='https://github.com/Science-of-Lojban/ncbi-taxonomy/',
+      author='Gleki',
+      license='MIT',
+      install_requires=[
+          'ete3',
+      ]
+      )
